8923454c15065aac7928f0a8e10ab46a706f5aa3
sachavincent <sacha.vincent@univ-tlse3.fr>
2020-04-30 03:15:40 +0200

    Connexion & autres

--- Android/app/src/main/java/fr/gendarmerienationale/reseauprevention31/asynctask/APICaller.java
+++ Android/app/src/main/java/fr/gendarmerienationale/reseauprevention31/asynctask/APICaller.java
@@ -1,206 +1,138 @@
 package fr.gendarmerienationale.reseauprevention31.asynctask;
 
+import static fr.gendarmerienationale.reseauprevention31.util.Tools.LOG;
+import static fr.gendarmerienationale.reseauprevention31.util.Tools.writeTraceException;
+
 import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.Toast;
 import fr.gendarmerienationale.reseauprevention31.R;
+import fr.gendarmerienationale.reseauprevention31.dialog.RequestConnectionDialog;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.lang.ref.WeakReference;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.Iterator;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-/**
- * Cette classe permet d'envoyer des requêtes au serveur et de
- * gérer sa réponse
- */
 public class APICaller extends AsyncTask<Void, Void, Boolean> {
 
-	private final WeakReference<Context> mContext;
+    private final WeakReference<Context> mContext;
 
-	private String mUser, mPassword, mIdDevice, mVersionApp, mVersionDev, mModelDev, mToken, mStrRep;
+    private String mKeyID, mStrRep;
 
+    private final static String URL = "";
 
-	private final WeakReference<RequestConnectionDialog> mDialog;
+    private final WeakReference<RequestConnectionDialog> mDialog;
 
-	public APICaller(String _user, String _password, String _idDevice, String _versionApp, String _versionDev, String _modelDev, Context _context, RequestConnectionDialog _dialog) {
-		mUser = _user;
-		mPassword = _password;
-		mIdDevice = _idDevice;
-		mVersionApp = _versionApp;
-		mVersionDev = _versionDev;
-		mModelDev = _modelDev;
+    public APICaller(String _keyID, Context _context, RequestConnectionDialog _dialog) {
+        mKeyID = _keyID;
 
-		mContext = new WeakReference<>(_context);
-		mDialog = new WeakReference<>(_dialog);
-	}
+        mContext = new WeakReference<>(_context);
+        mDialog = new WeakReference<>(_dialog);
+    }
 
-	@Override
-	protected void onPreExecute() {
-		super.onPreExecute();
-	}
+    @Override
+    protected Boolean doInBackground(Void... params) {
+        String s;
+        StringBuilder chaine = new StringBuilder();
 
+        HttpURLConnection httpConnection = null;
+        OutputStreamWriter writer = null;
+        InputStream stream = null;
+        BufferedReader buffer = null;
+        try {
+            URL url = new URL(URL);
+            httpConnection = (HttpURLConnection) url.openConnection();
+            httpConnection.setRequestMethod("POST");
+            httpConnection.setDoOutput(true);
+            httpConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            httpConnection.connect();
 
-	@Override
-	protected Boolean doInBackground(Void... params) {
-		String s;
-		StringBuilder chaine = new StringBuilder();
+            // ajout de la clé d'identification
+            writer = new OutputStreamWriter(httpConnection.getOutputStream());
+            writer.write("cle_identification=" + mKeyID);
+            writer.flush();
 
-		String urlParameters = mContext.get().getString(R.string.web_http_post_ident).replace(" ", "&");
-		urlParameters = urlParameters.replace("XXX", mUser);
-		urlParameters = urlParameters.replace("YYY", mPassword);
-		urlParameters = urlParameters.replace("ZZZ", mIdDevice);
-		urlParameters = urlParameters.replace("AAA", mVersionApp);
-		urlParameters = urlParameters.replace("BBB", mVersionDev);
-		urlParameters = urlParameters.replace("CCC", mModelDev);
+            int codeRep = httpConnection.getResponseCode();
+            if (codeRep == HttpURLConnection.HTTP_OK) {
+                stream = httpConnection.getInputStream();
+                buffer = new BufferedReader(new InputStreamReader(stream));
 
-		try {
-			URL url = new URL("https://api.tourgest.net/v3/auth");
-			URLConnection connection = url.openConnection();
-			HttpURLConnection httpConnection = (HttpURLConnection) connection;
-			httpConnection.setRequestMethod("POST");
-			httpConnection.setDoOutput(true);
-			httpConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-			httpConnection.connect();
+                while ((s = buffer.readLine()) != null)
+                    chaine.append(s);
+            } else {
+                mStrRep = translateErrHttp(mContext.get(), codeRep);
+                Log.w(LOG, mStrRep);
 
-			// ajout des paramètres
-			OutputStreamWriter writer = new OutputStreamWriter(httpConnection.getOutputStream());
-			writer.write(urlParameters);
-			writer.flush();
+                return false;
+            }
 
-			int codeRep = httpConnection.getResponseCode();
-			if (codeRep == HttpURLConnection.HTTP_OK) {
-				InputStream stream = httpConnection.getInputStream();
-				BufferedReader buffer = new BufferedReader(new InputStreamReader(stream));
-				while ((s = buffer.readLine()) != null)
-					chaine.append(s);
+            return true;
+        } catch (Exception e) {
+            Log.w(LOG, e.getMessage());
+            writeTraceException(e);
+        } finally {
+            try {
+                if (httpConnection != null)
+                    httpConnection.disconnect();
 
-				Log.d(LOG, chaine.toString());
-				JSonIdent ident = extractTourGestWebIdent(mContext.get(), chaine.toString());
-				if (ident == null) return false;
+                if (writer != null)
+                    writer.close();
 
-				if (ident.getError() == null)
-					mToken = ident.getToken();
-				else {
-					mStrRep = translateErrIdent(ident.getError());
+                if (buffer != null)
+                    buffer.close();
 
-					return false;
-				}
-			} else {
-				mStrRep = translateErrHttp(mContext.get(), codeRep);
-				Log.w(LOG, mStrRep);
+                if (stream != null)
+                    stream.close();
+            } catch (IOException e) {
+                Log.w(LOG, e.getMessage());
+                writeTraceException(e);
+            }
+        }
 
-				return false;
-			}
-			writer.close();
+        return false;
+    }
 
-			if (httpConnection != null)
-				httpConnection.disconnect();
+    @Override
+    protected void onPostExecute(Boolean result) {
+        super.onPostExecute(result);
 
-			return true;
-		} catch (Exception e) {
-			Log.w(LOG, e.getMessage());
-			writeTraceException(e);
-		}
+        if (result && mDialog.get() != null) {
+            mDialog.get().dismiss();
 
-		return false;
-	}
+            Toast.makeText(mContext.get(), mContext.get().getString(R.string.connection_successful), Toast.LENGTH_LONG)
+                    .show();
+        } else {
+            Toast.makeText(mContext.get(),
+                    mStrRep != null ? mStrRep : mContext.get().getString(R.string.connection_unsuccessful),
+                    Toast.LENGTH_SHORT).show();
 
-	@Override
-	protected void onPostExecute(Boolean result) {
-		super.onPostExecute(result);
+            mDialog.get().switchValiderState();
+        }
+    }
 
-		if (mToken != null && !mToken.isEmpty() && result && mDialog.get() != null) {
-			mDialog.get().dismiss();
+    private static String translateErrHttp(Context _ctx, int _code) {
+        String strErr = _ctx.getString(R.string.err_http_connect) + " - " + _code;
 
-			Toast.makeText(mContext.get(), mContext.get().getString(R.string.connection_successful), Toast.LENGTH_LONG).show();
-		} else {
-			Toast.makeText(mContext.get(), mStrRep != null ? mStrRep : mContext.get().getString(R.string.connection_unsuccessful), Toast.LENGTH_SHORT).show();
+        switch (_code) {
+            case 401:
+                strErr = _ctx.getString(R.string.err_http_auth_requise);
+                break;
+            case 403:
+                strErr = _ctx.getString(R.string.err_http_acces_interdit);
+                break;
+            case 404:
+                strErr = _ctx.getString(R.string.err_http_page_non_trouvee);
+                break;
+            case 500:
+                strErr = _ctx.getString(R.string.err_http_err_serveur_interne);
+                break;
+        }
 
-			mDialog.get().switchValiderState();
-		}
-	}
-
-	private String translateErrIdent(String _err) {
-		String strErr = _err;
-
-		switch (_err) {
-			case "100":
-				strErr = mContext.get().getString(R.string.err_http_serveur_err);
-				break;
-			case "101":
-				strErr = mContext.get().getString(R.string.err_http_username_password_invalid);
-				break;
-			case "102":
-				strErr = mContext.get().getString(R.string.err_http_licence_utilisee);
-				break;
-			case "103":
-				strErr = mContext.get().getString(R.string.err_http_compte_expire);
-				break;
-			case "104":
-				strErr = mContext.get().getString(R.string.err_http_app_invalid);
-				break;
-		}
-
-		return strErr;
-	}
-
-	private static JSonIdent extractTourGestWebIdent(Context _context, String _strReponse) {
-		JSonIdent ident = new JSonIdent();
-
-		try {
-			JSONObject reader = new JSONObject(_strReponse);
-			Iterator<String> keys = reader.keys();
-			while (keys.hasNext()) {
-				String key = keys.next();
-				if (key.equals(_context.getString(R.string.json_error))) {
-					ident.setError(reader.getString(key));
-				} else if (key.equals(_context.getString(R.string.json_ident_token))) {
-					ident.setToken(reader.getString(key));
-				} else if (key.equals(_context.getString(R.string.json_ident_releveur_identity))) {
-					ident.setReleveurIdentitiy(reader.getString(key));
-				} else if (key.equals(_context.getString(R.string.json_ident_regie_name))) {
-					ident.setRegieName(reader.getString(key));
-				} else if (key.equals(_context.getString(R.string.json_ident_expiration))) {
-					ident.setExpirationDate(reader.getString(key));
-				} else {
-					Log.d(LOG, "Key=" + key);
-				}
-			}
-		} catch (JSONException e) {
-			Log.w(LOG, e.getMessage());
-			writeTraceException(e);
-			return null;
-		}
-
-		return ident;
-	}
-
-	private static String translateErrHttp(Context _ctx, int _code) {
-		String strErr = _ctx.getString(R.string.err_http_connect) + " - " + String.valueOf(_code);
-
-		switch (_code) {
-			case 401:
-				strErr = _ctx.getString(R.string.err_http_auth_requise);
-				break;
-			case 403:
-				strErr = _ctx.getString(R.string.err_http_acces_interdit);
-				break;
-			case 404:
-				strErr = _ctx.getString(R.string.err_http_page_non_trouvee);
-				break;
-			case 500:
-				strErr = _ctx.getString(R.string.err_http_err_serveur_interne);
-				break;
-		}
-
-		return strErr;
-	}
+        return strErr;
+    }
 }
