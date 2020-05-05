5d5c53cb8592215cc5085e7fce813641e1395266
godefroyChabanas <godefroy.chabanas@univ-tlse3.fr>
2020-05-05 17:48:46 +0200

    envoyer un message

--- Android/app/src/main/java/fr/gendarmerienationale/reseauprevention31/activity/AccueilActivity.java
+++ Android/app/src/main/java/fr/gendarmerienationale/reseauprevention31/activity/AccueilActivity.java
@@ -1,6 +1,8 @@
 package fr.gendarmerienationale.reseauprevention31.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import fr.gendarmerienationale.reseauprevention31.R;
@@ -11,7 +13,32 @@ public class AccueilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_accueil);
+        Button buttonEnvoyerMessage = findViewById(R.id.ButtonEnvoyerMessage);
+        Button buttonConsulterInfos = findViewById(R.id.ButtonConsulterInfos);
+        Button buttonLocaliserBrigade;
+        Button buttonConseilsProtection;
+
+        if(MainActivity.sDatabaseHelper.isUserConnected()) {
+            setContentView(R.layout.activity_accueil_connecte);
+
+            buttonLocaliserBrigade = findViewById(R.id.buttonLocaliserBrigade);
+            buttonConseilsProtection = findViewById(R.id.ButtonConseilsProtection);
+
+            buttonEnvoyerMessage.setOnClickListener(v -> startActivity(new Intent(AccueilActivity.this, EnvoyerMessageActivity.class)));
+            buttonConsulterInfos.setOnClickListener(v -> startActivity(new Intent(AccueilActivity.this, ConsulterMessagesActivity.class)));
+            buttonConseilsProtection.setOnClickListener(v -> startActivity(new Intent(AccueilActivity.this, ConseilsProctectionActivity.class)));
+            buttonLocaliserBrigade.setOnClickListener(v -> startActivity(new Intent(AccueilActivity.this, LocaliserBrigadeActivity.class)));
+
+        }else{
+            setContentView(R.layout.activity_accueil_non_connecte);
+
+            buttonLocaliserBrigade = findViewById(R.id.buttonLocaliserBrigade);
+            buttonConseilsProtection = findViewById(R.id.ButtonConseilsProtection);
+
+            buttonConseilsProtection.setOnClickListener(v -> startActivity(new Intent(AccueilActivity.this, ConseilsProctectionActivity.class)));
+            buttonLocaliserBrigade.setOnClickListener(v -> startActivity(new Intent(AccueilActivity.this, LocaliserBrigadeActivity.class)));
+
+        }
 
         ImageView imageView = findViewById(R.id.imageChambre);
 
--- Android/app/src/main/java/fr/gendarmerienationale/reseauprevention31/activity/MainActivity.java
+++ Android/app/src/main/java/fr/gendarmerienationale/reseauprevention31/activity/MainActivity.java
@@ -49,6 +49,8 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
                     .requestPermissions(this, getString(R.string.permissions_accepter), PERMS_CALLBACK, appPerms);
     }
 
+
+
     /**
      * Initialise l'appli
      */
@@ -80,7 +82,7 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
         });
 
         buttonConnexionAnonyme
-                .setOnClickListener(v -> startActivity(new Intent(MainActivity.this, AccueilActivity_non_connecte.class)));
+                .setOnClickListener(v -> startActivity(new Intent(MainActivity.this, AccueilActivity.class)));
 
     }
 
