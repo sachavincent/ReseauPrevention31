9683e8836b17d21d4ad2acd937ba25e7435f067a
godefroyChabanas <godefroy.chabanas@univ-tlse3.fr>
2020-05-08 15:06:03 +0200

    modifs

--- Android/app/src/main/java/fr/gendarmerienationale/reseauprevention31/activity/AccueilActivity.java
+++ Android/app/src/main/java/fr/gendarmerienationale/reseauprevention31/activity/AccueilActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
@@ -31,29 +32,31 @@ public class AccueilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        Button buttonEnvoyerMessage = findViewById(R.id.buttonEnvoyerMessage);
-//        Button buttonConsulterInfos = findViewById(R.id.buttonConsulterInfos);
+//        Button buttonEnvoyerMessage;
+//        Button buttonConsulterMessage;
 //        Button buttonLocaliserBrigade;
 //        Button buttonConseilsProtection;
-//
+
         if (this.mConnected) {
             setContentView(R.layout.activity_accueil_connecte);
-//
+
+//            buttonEnvoyerMessage = findViewById(R.id.buttonEnvoyerMessage);
+//            buttonConsulterMessage = findViewById(R.id.buttonConsulterMessage);
 //            buttonLocaliserBrigade = findViewById(R.id.buttonLocaliserBrigade);
-//            buttonConseilsProtection = findViewById(R.id.ButtonConseilsProtection);
-//
-//            buttonEnvoyerMessage.setOnClickListener(
-//                    v -> startActivity(new Intent(AccueilActivity.this, EnvoyerMessageActivity.class)));
-//            buttonConsulterInfos.setOnClickListener(
+//            buttonConseilsProtection = findViewById(R.id.buttonConseilsProtection);
+
+//            buttonConsulterMessage.setOnClickListener(
 //                    v -> startActivity(new Intent(AccueilActivity.this, ConsulterMessagesActivity.class)));
 //            buttonConseilsProtection.setOnClickListener(
 //                    v -> startActivity(new Intent(AccueilActivity.this, ConseilsProctectionActivity.class)));
 //            buttonLocaliserBrigade.setOnClickListener(
 //                    v -> startActivity(new Intent(AccueilActivity.this, LocaliserBrigadeActivity.class)));
-//
+
         } else {
             setContentView(R.layout.activity_accueil_connecte);
 //
+//            buttonEnvoyerMessage = findViewById(R.id.buttonEnvoyerMessage);
+//            buttonConsulterMessage = findViewById(R.id.buttonConsulterMessage);
 //            buttonLocaliserBrigade = findViewById(R.id.buttonLocaliserBrigade);
 //            buttonConseilsProtection = findViewById(R.id.ButtonConseilsProtection);
 //
@@ -63,84 +66,79 @@ public class AccueilActivity extends AppCompatActivity {
 //                    v -> startActivity(new Intent(AccueilActivity.this, LocaliserBrigadeActivity.class)));
 //
         }
-            mActionBar = getSupportActionBar();
-            if (mActionBar != null) {
-                mActionBar.setDisplayHomeAsUpEnabled(!mConnected);
-                mActionBar.setSubtitle(mConnected ? R.string.accueil : R.string.accueil_anonyme);
-            }
-//
-//        ImageView imageView = findViewById(R.id.imageChambre);
-//
-//        imageView.setImageResource(R.mipmap.cci);
-
-        }
-
-        @Override
-        public boolean onSupportNavigateUp () {
-            AccueilActivity.super.onBackPressed();
-
-            return true;
-        }
-
-        @Override
-        public boolean onCreateOptionsMenu (Menu menu){
-            getMenuInflater().inflate(R.menu.menu, menu);
-
-            this.mLogoutItem = menu.findItem(R.id.logout);
-            // La déconnexion n'est visible que si l'utilisateur est connecté
-            this.mLogoutItem.setVisible(mConnected);
-
-            return true;
-        }
-
-        @Override
-        protected void onResume () {
-            super.onResume();
-
-            if (this.mLogoutItem != null) {
-                this.mConnected = MainActivity.sDatabaseHelper.isUserConnected();
-                this.mLogoutItem.setVisible(this.mConnected);
-            }
-
-            if (mActionBar != null) {
-                mActionBar.setDisplayHomeAsUpEnabled(!mConnected);
-                mActionBar.setSubtitle(mConnected ? R.string.accueil : R.string.accueil_anonyme);
-            }
-        }
-
-        @Override
-        public boolean onOptionsItemSelected (MenuItem item){
-            int id = item.getItemId();
-
-            if (id == R.id.logout) {
-                new AlertDialog.Builder(this)
-                        .setTitle("Voulez-vous vous déconnecter ?")
-                        .setIcon(R.drawable.logout_black)
-                        .setNegativeButton(R.string.non, (dialog, which) -> dialog.dismiss())
-                        .setPositiveButton(R.string.oui, (dialog, which) -> disconnectUser())
-                        .show();
-            }
-
-
-            return super.onOptionsItemSelected(item);
-        }
-
-        private void disconnectUser () {
-            if (MainActivity.sDatabaseHelper.deleteUser()) {
-                Toast.makeText(this, R.string.deconnexion_reussie, Toast.LENGTH_SHORT).show();
-
-                Intent intent = new Intent(AccueilActivity.this, MainActivity.class);
-
-                finish();
-                startActivity(intent);
-            }
-        }
-
-
-        @Override
-        public void onBackPressed () {
-            // Si l'utilisateur n'est pas connecté, lui permettre de revenir à l'activité de connexion (MainActivity)
-            if (!mConnected)
-                super.onBackPressed();
+        mActionBar = getSupportActionBar();
+        if (mActionBar != null) {
+            mActionBar.setDisplayHomeAsUpEnabled(!mConnected);
+            mActionBar.setSubtitle(mConnected ? R.string.accueil : R.string.accueil_anonyme);
         }
     }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        AccueilActivity.super.onBackPressed();
+
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        this.mLogoutItem = menu.findItem(R.id.logout);
+        // La déconnexion n'est visible que si l'utilisateur est connecté
+        this.mLogoutItem.setVisible(mConnected);
+
+        return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (this.mLogoutItem != null) {
+            this.mConnected = MainActivity.sDatabaseHelper.isUserConnected();
+            this.mLogoutItem.setVisible(this.mConnected);
+        }
+
+        if (mActionBar != null) {
+            mActionBar.setDisplayHomeAsUpEnabled(!mConnected);
+            mActionBar.setSubtitle(mConnected ? R.string.accueil : R.string.accueil_anonyme);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.logout) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Voulez-vous vous déconnecter ?")
+                    .setIcon(R.drawable.logout_black)
+                    .setNegativeButton(R.string.non, (dialog, which) -> dialog.dismiss())
+                    .setPositiveButton(R.string.oui, (dialog, which) -> disconnectUser())
+                    .show();
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void disconnectUser() {
+        if (MainActivity.sDatabaseHelper.deleteUser()) {
+            Toast.makeText(this, R.string.deconnexion_reussie, Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(AccueilActivity.this, MainActivity.class);
+
+            finish();
+            startActivity(intent);
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        // Si l'utilisateur n'est pas connecté, lui permettre de revenir à l'activité de connexion (MainActivity)
+        if (!mConnected)
+            super.onBackPressed();
+    }
+}
