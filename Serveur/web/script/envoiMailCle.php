05819c13c4c7d82d6004efa9e5ff54ae1b48ae16
sachavincent <sacha.vincent@univ-tlse3.fr>
2020-06-07 13:22:55 +0200

    mail cle

--- Serveur/web/script/envoiMailCle.php
+++ Serveur/web/script/envoiMailCle.php
@@ -33,8 +33,6 @@ $body = [
 $response = $mj->post(Resources::$Email, ['body' => $body]); */
 //$response->success() && var_dump($response->getData());
 
-shell_exec ("echo -e 
-    'Bonjour,\nsuite à votre inscription sur l'application prevention31 voici votre clé de connexion unique: \n\n" . $cle 
-    . "\n\nPour vous connecter sur l\'application :\n   - Copiez la cle\n   - Rendez-vous sur l\'application\n  - Selectionnez connexion puis collez la clé\n   - Enfin validez, vous voila maintenant sur votre espace\n\n
-    L\'équipe RéseauPrévention31' | mail -s 'Validation de votre inscription' " . $infoUtilisateur['mail']);
+shell_exec ("echo 'Bonjour,\n\nsuite à votre inscription sur l`application prevention31 voici votre clé de connexion unique: \n\n" . $cle 
+	. "\n\nPour vous connecter sur l`application :\n	- Copiez la cle\n	- Rendez-vous sur l`application\n	- Selectionnez connexion puis collez la clé\n	- Enfin validez, vous voilà maintenant sur votre espace\n\nL`équipe RéseauPrévention31' | mail -s 'Validation de votre inscription' " . $infoUtilisateur['mail']);
 ?>
