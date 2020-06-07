2d46c3db0fb24ef4af3c5f2b95e499d96a14d211
Thomas Appriou <thomas.appriou@univ-tlse3.fr>
2020-06-07 13:35:17 +0200

    mail Cle

--- Serveur/web/script/envoiMailAnnonce.php
+++ Serveur/web/script/envoiMailAnnonce.php
@@ -3,6 +3,7 @@ $objet = strip_tags($_POST['input-objet-annonce']);
 $texte = strip_tags($_POST['texte']);
 
 foreach($listeDestinataire as $dest){
-    shell_exec("echo " . $texte . " | mail -s '" . $objet . "' " . $dest['mail']); 
+    shell_exec("echo '" . $texte . "' | mail -s '" . $objet . "' " . $dest['mail']); 
 }
 ?>
+
