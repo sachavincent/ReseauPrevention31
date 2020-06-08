c8477618125aabfdb657441bf4d4afc8efa1915b
Thomas Appriou <thomas.appriou@univ-tlse3.fr>
2020-06-08 15:14:32 +0200

    nouvelle interface

--- Serveur/web/script/envoiMailAnnonce 2.php
+++
@@ -1,9 +0,0 @@
-<?php
-$objet = strip_tags($_POST['input-objet-annonce']);
-$texte = strip_tags($_POST['texte']);
-
-foreach($listeDestinataire as $dest){
-    shell_exec("echo '" . $texte . "' | mail -s '" . $objet . "' " . $dest['mail']); 
-}
-?>
-
--- Serveur/web/script/envoiMailCle 2.php
+++
@@ -1,9 +0,0 @@
-<?php
-$cle = $infoUtilisateur['cle'];
-$requete = $bdd->prepare('SELECT nomUtilisateur, prenomUtilisateur, mail FROM Utilisateur WHERE cle = ?');
-$requete->execute(array($cle));
-$infoUtilisateur = $requete->fetch();
-
-shell_exec ("echo 'Bonjour,\n\nsuite à votre inscription sur l`application prevention31 voici votre clé de connexion unique: \n\n" . $cle 
-	. "\n\nPour vous connecter sur l`application :\n	- Copiez la cle\n	- Rendez-vous sur l`application\n	- Selectionnez connexion puis collez la clé\n	- Enfin validez, vous voilà maintenant sur votre espace\n\nL`équipe RéseauPrévention31' | mail -s 'Validation de votre inscription' " . $infoUtilisateur['mail']);
-?>
